444be5fd8551968e3618d6c37b0bf79c26f9bd5d
gefeiyang <variousdid@gmail.com>
2024-09-30 16:36:51 +0800

    update xhs

--- api/index.ts
+++ api/index.ts
@@ -1,13 +1,41 @@
-import express, { Express, Request, Response } from 'express';
+import express, {Request, Response} from 'express';
+import { convertToMp3, getMp4 } from "../src/util";
+  
+const app = express();
+const port = process.env.PORT || 3000;
 
-const app: Express = express();
+app.use(express.json());
 
-app.get('/api', (req: Request, res: Response) => {
-  res.send('Hello from Vercel Serverless Function!');
-});
+app.post('/convert', async (req: Request, res: Response):Promise<any> => {
+  try {
+    const { url } = req.body;
+    if (!url) {
+      return res.status(400).json({ error: 'URL is required' });
+    }
 
-app.get('/api/test', (req: Request, res: Response) => {
-  res.json({ message: 'This is a test endpoint' });
+    // 获取视频文件.mp4和链接标题
+    const mp4File = await getMp4(url);
+    if (!mp4File?.url) {
+      return res.status(404).json({ error: 'No MP4 link found' });
+    }
+    // 将上一步的.mp4转换成.mp3文件
+    const mp3File = await convertToMp3(mp4File)
+
+    // 服务端下载
+    // const downloadedFilePath = await downloadMp3(mp3File, mp4File?.fileName);
+
+    res.json({
+      message: 'File converted and downloaded successfully',
+      code: '200',
+      result: {
+        fileName: mp4File?.fileName,
+        url: mp3File?.url
+      }
+    });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred during processing' });
+  }
 });
 
 export default app;
